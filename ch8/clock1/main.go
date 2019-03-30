537f1abae55291a038fac52d75acb3e078e6c21d
Arnab <arnab.ee.ju@gmail.com>
2019-03-30 19:57:19 +0530

    Completed some basics of goroutine

---
+++ ch8/clock1/main.go
@@ -0,0 +1,36 @@
+// Clock1 is a TCP server that periodically writes the time
+
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn) // handle one connection at a time
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("16:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
